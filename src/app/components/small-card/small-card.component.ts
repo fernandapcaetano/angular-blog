7dcc261c77a3f704f248c1676a454feaa2264b11
Maria Fernanda <fernandamariacaetanopereira@gmail.com>
2024-01-13 16:15:38 -0300

    Tornando dinâmico

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = 'https://cordierinvestimentos.com.br/wp-content/uploads/2020/09/placeholder.png'
+  @Input()
+  author:string = ''
+  @Input()
+  cardTitle:string = ''
 
   constructor() { }
 
